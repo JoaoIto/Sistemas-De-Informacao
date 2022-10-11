d5d80043f56d5baff2c5c3464b8f483c6469d038
João Victor <78181193+JoaoIto@users.noreply.github.com>
2022-10-11 10:18:57 -0300

    Update Qt1.java

---
+++ 1Período/Algoritmos/exercicios/Arrays/Lista1/Qt1.java
@@ -0,0 +1,21 @@
+/* *01 - Escreva um programa que permita ao usuário
+preencher um array de 10 inteiros e ao final apresente
+o dobro de cada valor contido no array*
+*/
+
+import java.util.Scanner;
+public class Qt1 {
+    public static void main (String args[]){
+        Scanner leitor = new Scanner(System.in);
+        int[] arr;
+        arr = new int[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Defina um valor: ");
+            arr[i] = leitor.nextInt();
+        }
+        for(int i = 0; i < arr.length; i++){
+            System.out.println("Os valores do array são: "+ arr[i]*2);
+        }
+    }
+}

9d8db402c298ee9aaf8318a9fd12e84ae35147d7
Lorenzo <joaovictorpfr@gmail.com>
2022-11-21 11:08:20 -0300

    Att teste para exam

---
+++ 1Período/Algoritmos/exercicios/Classes/Final/Ex.java
@@ -0,0 +1,34 @@
+/* 01 - Considere uma academia de musculação que realiza um acompanhamento
+dos alunos matriculados. Para cada aluno são armazenados
+o nome, idade, peso, altura, a quanto tempo terina (em meses),
+tipo de treino e o dia da matrícula.
+
+Escreva um programa que contenha funções que
+permitam cadastrar um aluno (com o limite de 100 alunos),
+atualizar o tempo de treino de todos os alunos que se matricularam em um determinado dia,
+consultar os dados de um aluno (pesquisando pelo número),
+calcular o IMC de um aluno e
+atualizar o tipo de treino de todos os alunos (até 3 meses: iniciante,
+entre 4 e 6 meses: intermediário,
+entre 7 meses e 1 ano: marombeiro junior,
+entre 1 e 2 anos: marombeiro sênior,
+acima de 2 anos: Arnold Schwarzenegger.
+*/+
+
+import java.util.Scanner;
+
+class Registro{
+    String nome, tipoTreino;
+    int idade, tempoTreino, diaMatricula;
+    double altura, peso;
+}
+
+public class Main {
+
+    public static String Cadastro(Registro pessoa){
+
+    }
+    public static void main(String[]args){
+        Scanner Leitor = new Scanner(System.in);
+    }
+}

81a7dd8d1444e9247f6eb43b5929e0dc654ff524
João <joaovictorpfr@gmail.com>
2023-03-06 19:38:51 -0300

    Add exercises java

---
+++ 2Período/Algoritmos2/exercicios/alg2-atv4/maquinaRefrigerante/src/classes/MaquinaRefrigerante.java
@@ -0,0 +1,10 @@
+package classes;
+import classes.Produtos;
+
+public class MaquinaRefrigerante {
+	Produtos[] lista = new Produtos[10];
+	protected Integer escolhaCliente;
+	protected String formaPagamento;
+	protected Double qntdDinheiro;
+}
+
---
+++ 2Período/Algoritmos2/exercicios/alg2-atv4/maquinaRefrigerante/src/classes/Produtos.java
@@ -0,0 +1,8 @@
+package classes;
+
+public class Produtos {
+	protected Integer codigoVenda;
+	protected String nome;
+	protected Integer qntdProduto;
+	protected Double preco;
+}

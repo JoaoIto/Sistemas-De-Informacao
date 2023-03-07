6714ce7977d12de19d4b2840fb2525e9c5b5ac5f
João <joaovictorpfr@gmail.com>
2023-03-07 04:50:47 -0300

    Add final version complete av4-alg2

--- 2Período/Algoritmos2/exercicios/alg2-atv4/maquinaRefrigerante/src/app/main.java
+++ 2Período/Algoritmos2/exercicios/alg2-atv4/maquinaRefrigerante/src/app/main.java
@@ -1,72 +1,31 @@
 package app;
 import java.util.Scanner;
 import classes.MaquinaRefrigerante;
+import java.util.ArrayList;
+import java.util.List;
 
 public class main {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		MaquinaRefrigerante[] refrigerantes = new MaquinaRefrigerante[5];
-		MaquinaRefrigerante refrigerante = new MaquinaRefrigerante();
-		
-		refrigerantes[0] = new MaquinaRefrigerante("Coca_600ml", 15.0);
-		refrigerantes[1] = new MaquinaRefrigerante("Pepsi_ks", 12.0);
-		refrigerantes[2] = new MaquinaRefrigerante("Guaraná-Jesus_Lata", 6.0);
-		refrigerantes[3] = new MaquinaRefrigerante("H2O_250ml", 7.0);
-		refrigerantes[4] = new MaquinaRefrigerante("Red-Bull_Lata", 9.0);
-	
-		System.out.println("Digite aqui o refri de sua escolha: ");
-		
-		for(int i = 0; i < refrigerantes.length; i++) {
-			System.out.println(i + "O refrigerante: " + refrigerantes[i].getNome());
-		}
-		
-		int escolhaRefri = scan.nextInt();
-		
-		switch (escolhaRefri) {
-		case 1: {
-			return refrigerantes[0].getPreco();
-			break;
-		}
-		case 2:{
-			return refrigerantes[1].getPreco();
-		}case 3:{
-			return refrigerantes[2].getPreco();
-		}case 4:{
-			return refrigerantes[3].getPreco();
-		}case 5:{
-			return refrigerantes[4].getPreco();
-		}
-		default: {
-			System.out.println("Codigo invalido!");
-		}
-		
-		System.out.println("Qual a sua forma de pagamento:\nDigite 1 para dinheiro ou pix, e 2 para cartão ");
-		int escolha = scan.nextInt();
-		
-		System.out.println("O refrigerante custa: " + refrigerantes[MaquinaRefrigerante].getPreco());
-		System.out.println("Insira o dinheiro requerido: ");
-		refrigerante.setDinheiro(scan.nextDouble());
-		
-		if(escolha == 1) {
-			System.out.println("Forma de pagamento: Dinheiro/PIX");
-			
-			if(refrigerante.getDinheiro() >= refrigerantes[escolhaRefri].getPreco()) {
-				System.out.println("Troco: ");
-				System.out.println("Refrigerante liberado!");
-			}else {
-				System.out.println("Falta dinheiro para o refrigerante!");
-			}
-		}else {
-			System.out.println("Forma de pagamento: Cartão");
-			if(dinheiro >= 15.0) {
-				System.out.println("Troco: ");
-				System.out.println(dinheiro - 10);
-				System.out.println("Refrigerante liberado!");
-			}else {
-				System.out.println("Falta dinheiro para o refrigerante!");
-			}
-		}
-	}
+        Scanner sc = new Scanner(System.in);
+
+
+        MaquinaRefrigerante maquina = new MaquinaRefrigerante();
+        List<MaquinaRefrigerante> list = new ArrayList<>();
+
+        list.add(new MaquinaRefrigerante("Coca", 1, 15.00));
+        list.add(new MaquinaRefrigerante("Pepsi", 2, 12.00));
+        list.add(new MaquinaRefrigerante("GuaranÃ¡-Jesus", 3, 6.00));
+        list.add(new MaquinaRefrigerante("H2O_250ml", 4, 7.00));
+        list.add(new MaquinaRefrigerante("Red-Bull_Lata", 5, 9.00));
+
+        for (MaquinaRefrigerante var : list) {
+            System.out.printf("%d %s R$%.2f\n", var.getCodProduto(), var.getNome(), var.getPrecoProduto());
+        }
+        System.out.println();
+        System.out.println("Escolha um refrigerante e insira a quantia em dinheiro");
+        maquina.selecionarRefrigerante(list, sc.nextInt(), sc.nextDouble());
+
+
+    }
 }
--- 2Período/Algoritmos2/exercicios/alg2-atv4/maquinaRefrigerante/src/classes/MaquinaRefrigerante.java
+++ 2Período/Algoritmos2/exercicios/alg2-atv4/maquinaRefrigerante/src/classes/MaquinaRefrigerante.java
@@ -1,46 +1,74 @@
 package classes;
+import java.util.List;
 
 public class MaquinaRefrigerante {
-	private String nomeProduto;
-	private Double precoProduto;
-	private Double dinheiro;
-	
-	public MaquinaRefrigerante() {	
-	}
-	
-	public MaquinaRefrigerante(String nome, Double preco) {
-		super();
-		this.nomeProduto = nome;
-		this.precoProduto = preco;
-	}
-	
-	public String getNome() {
-		return nomeProduto;
-	}
-	
-	public void setNome(String nome) {
-		this.nomeProduto = nome;
-	}
-	
-	public Double getPreco() {
-		return precoProduto;
-	}
-	
-	public void setPreco(Double preco) {
-		this.precoProduto = preco;
-	}
-	
-	public void setDinheiro(Double dinheiro) {
-		this.dinheiro = dinheiro;
-	}
-	
-	public Double getDinheiro() {
-		return dinheiro;
-	}
-	
-	public void inserirDinheiro(Double dinheiro) {
-		this.dinheiro += dinheiro;
-	}
-	
-	
+
+
+    private String nomeProduto;
+    private int codProduto;
+    private double precoProduto;
+    private double dinheiro;
+
+    public MaquinaRefrigerante() {
+    }
+
+    public MaquinaRefrigerante(String nome, int codProduto, double precoProduto) {
+        this.nomeProduto = nome;
+        this.codProduto = codProduto;
+        this.precoProduto = precoProduto;
+    }
+
+    public String getNome() {
+        return nomeProduto;
+    }
+
+    public void setNome(String nome) {
+        this.nomeProduto = nome;
+    }
+
+    public int getCodProduto() {
+        return codProduto;
+    }
+
+    public void setCodProduto(int codProduto) {
+        this.codProduto = codProduto;
+    }
+
+
+    public double getPrecoProduto() {
+        return precoProduto;
+    }
+
+    public void setPrecoProduto(double precoProduto) {
+        this.precoProduto = precoProduto;
+    }
+
+    public void setDinheiro(double dinheiro) {
+        this.dinheiro = dinheiro;
+    }
+
+    public double getDinheiro() {
+        return dinheiro;
+    }
+
+    public void inserirDinheiro(double dinheiro) {
+        this.dinheiro += dinheiro;
+    }
+    public void selecionarRefrigerante(List<MaquinaRefrigerante> lista, int codProduto, double dinheiro) {
+        for (MaquinaRefrigerante refrigerante : lista) {
+            if (refrigerante.getCodProduto() == codProduto) {
+                System.out.println("Você escolheu: " + refrigerante.getNome());
+                System.out.println("Preço: R$" + refrigerante.getPrecoProduto());
+                refrigerante.inserirDinheiro(dinheiro);
+                if (refrigerante.getDinheiro() >= refrigerante.getPrecoProduto()) {
+                    System.out.printf("Troco %.2fR$\n", (refrigerante.getDinheiro() - refrigerante.getPrecoProduto()));
+                    System.out.println("Venda realizada");
+                } else {
+                    System.out.println("Dinheiro insuficiente");
+                }
+            }
+        }
+    }
+
+
 }

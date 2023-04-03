894ad4a6a33e83101d26c2ae045fea6d64e2af82
João <joaovictorpfr@gmail.com>
2023-04-03 09:22:04 -0300

    Add em todos os tipos

--- 2Período/Algoritmos2/exercicios/Trabalhos/Final/classes/multiplicaCast.java
+++ 2Período/Algoritmos2/exercicios/Trabalhos/Final/classes/multiplicaCast.java
@@ -2,54 +2,70 @@ package classes;
 import classes.matematica;
 
 public class multiplicaCast {
-    // Criando classe para multiplicação, transformando valores para int
-    // Usando int e double;
-    public int multiplica(double n1, double n2){
-        int result = (int)(n1 * n2);
-        return result;
+
+    private matematica m = new matematica();
+
+    public int multiplica(int n1, int n2) {
+        return (int) m.multiplica(n1, n2);
     }
 
-    public int multiplica(int n1, int n2){
-        return n1 * n2;
+    public int multiplica(float n1, float n2) {
+        return (int) m.multiplica((int) n1, (int) n2);
     }
 
-    public int multiplica(double n1, int n2) {
-        int result = (int)n1 + n2;
-        return result;
+    public int multiplica(double n1, double n2) {
+        return (int) m.multiplica((int) n1, (int) n2);
     }
 
-    public int multiplica(int n1, double n2){
-        int result = n1 + (int)n2;
-        return result;
+    public int multiplica(long n1, long n2) {
+        return (int) m.multiplica((int) n1, (int) n2);
     }
 
-    public int multiplica(float n1, float n2){
-        int result = (int)(n1 * n2);
-        return result;
+    public int multiplica(Float n1, Float n2) {
+        return (int) m.multiplica((int) n1.floatValue(), (int) n2.floatValue());
+    }
+
+    public int multiplica(Double n1, Double n2) {
+        return (int) m.multiplica((int) n1.doubleValue(), (int) n2.doubleValue());
+    }
+
+    public int multiplica(int n1, float n2) {
+        return (int) m.multiplica(n1, (int) n2);
     }
 
     public int multiplica(float n1, int n2) {
-        int result = (int) n1 * n2;
-        return result;
+        return (int) m.multiplica((int) n1, n2);
     }
 
-    public int multiplica(int n1, float n2){
-        int result = n1 * (int)n2;
-        return result;
+    public int multiplica(int n1, double n2) {
+        return (int) m.multiplica(n1, (int) n2);
     }
 
-    public int multiplica(long n1, long n2){
-        int result = (int)(n1 * n2);
-        return result;
+    public int multiplica(double n1, int n2) {
+        return (int) m.multiplica((int) n1, n2);
     }
 
-    public int multiplica(long n1, int n2){
-        int result = (int)n1 * n2;
-        return result;
+    public int multiplica(int n1, long n2) {
+        return (int) m.multiplica(n1, (int) n2);
     }
 
-    public int multiplica(int n1, long n2){
-        int result = n1 * (int)n2;
-        return result;
+    public int multiplica(long n1, int n2) {
+        return (int) m.multiplica((int) n1, n2);
+    }
+
+    public int multiplica(int n1, Float n2) {
+        return (int) m.multiplica(n1, (int) n2.floatValue());
+    }
+
+    public int multiplica(Float n1, int n2) {
+        return (int) m.multiplica((int) n1.floatValue(), n2);
+    }
+
+    public int multiplica(int n1, Double n2) {
+        return (int) m.multiplica(n1, (int) n2.doubleValue());
+    }
+
+    public int multiplica(Double n1, int n2) {
+        return (int) m.multiplica((int) n1.doubleValue(), n2);
     }
 }

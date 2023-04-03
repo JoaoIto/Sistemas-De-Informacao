c02973059e150008406200c6d4deaa958305448c
João <joaovictorpfr@gmail.com>
2023-04-03 16:41:26 -0300

    Tirando responsabilidade de imprimir cada tipo de main

--- 2Período/Algoritmos2/exercicios/Trabalhos/Final/Questão 4/app/main.java
+++ 2Período/Algoritmos2/exercicios/Trabalhos/Final/Questão 4/app/main.java
@@ -10,12 +10,9 @@ public class main {
 
         // Criando pessoa e testando método imprimir principal;
         Pessoa pessoa = new Pessoa(1, "Joao", "joaovictorpfr@gmail.com");
-        System.out.println("Imprimindo pessoa:\n ");
 
         Pessoa pessoaFisica = new PessoaFisica(pessoa, "984207313", "02347982349");
-        System.out.println("Imprimindo pessoa física:\n ");
 
         Pessoa pessoaJuridica = new PessoaJuridica(pessoa, "282394629347", "13-TO", "3692478236");
-        System.out.println("Imprimindo pessoa jurídica:\n ");
     }
 }

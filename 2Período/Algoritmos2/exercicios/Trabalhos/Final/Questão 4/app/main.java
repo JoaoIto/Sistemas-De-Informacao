1e463dfb15459b60d7563af30471a8950d57ae4f
João <joaovictorpfr@gmail.com>
2023-04-03 16:22:14 -0300

    Add base 4

---
+++ 2Período/Algoritmos2/exercicios/Trabalhos/Final/Questão 4/app/main.java
@@ -0,0 +1,24 @@
+package app;
+
+import classes.Pessoa;
+import classes.PessoaFisica;
+import classes.PessoaJuridica;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // Criando pessoa e testando método imprimir principal;
+        Pessoa pessoa = new Pessoa(1, "Joao", "joaovictorpfr@gmail.com");
+        System.out.println("Imprimindo pessoa:\n ");
+        pessoa.imprimir();
+
+        Pessoa pessoaFisica = new PessoaFisica(pessoa, "984207313", "02347982349");
+        System.out.println("Imprimindo pessoa física:\n ");
+        pessoaFisica.imprimir();
+
+        Pessoa pessoaJuridica = new PessoaJuridica(pessoa, "282394629347", "13-TO", "3692478236");
+        System.out.println("Imprimindo pessoa jurídica:\n ");
+        pessoaJuridica.imprimir();
+    }
+}

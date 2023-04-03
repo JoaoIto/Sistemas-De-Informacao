d701b4d3ba560ca87e2e601cb34432cb8ac2168d
João <joaovictorpfr@gmail.com>
2023-04-03 09:11:00 -0300

    Add int float

--- 2Período/Algoritmos2/exercicios/Trabalhos/Final/classes/multiplicaCast.java
+++ 2Período/Algoritmos2/exercicios/Trabalhos/Final/classes/multiplicaCast.java
@@ -3,6 +3,7 @@ import classes.matematica;
 
 public class multiplicaCast {
     // Criando classe para multiplicação, transformando valores para int
+    // Usando int e double;
     public int multiplica(double n1, double n2){
         int result = (int)(n1 * n2);
         return result;
@@ -13,12 +14,27 @@ public class multiplicaCast {
     }
 
     public int multiplica(double n1, int n2) {
-        int result = (int) n1 + n2;
+        int result = (int)n1 + n2;
         return result;
     }
 
     public int multiplica(int n1, double n2){
         int result = n1 + (int)n2;
+        return result;
+    }
+
+    public int multiplica(float n1, float n2){
+        int result = (int)(n1 * n2);
+        return result;
+    }
+
+    public int multiplica(float n1, int n2) {
+        int result = (int) n1 * n2;
+        return result;
+    }
+
+    public int multiplica(int n1, float n2){
+        int result = n1 * (int)n2;
         return result;
     }
 }

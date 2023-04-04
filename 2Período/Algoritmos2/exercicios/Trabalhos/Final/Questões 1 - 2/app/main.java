6b75b8b24996e0ebc806dbf0a596f126c516b3f2
João <joaovictorpfr@gmail.com>
2023-04-04 09:09:29 -0300

    Testando com mais operações possíveis

--- 2Período/Algoritmos2/exercicios/Trabalhos/Final/Questões 1 - 2/app/main.java
+++ 2Período/Algoritmos2/exercicios/Trabalhos/Final/Questões 1 - 2/app/main.java
@@ -12,19 +12,45 @@ public class main {
         matematica mat = new matematica();
 
         // Testando os métodos de operações matemáticas
+        // Variáveis números:
         double n1 = 10.5;
         double n2 = 5.2;
+        double n3 = 2;
+        double n4 = 3;
+
+        // Variáveis somas:
         double resultadoSoma = mat.soma(n1, n2);
+        double resultadoSoma2 = mat.soma(n3, n4);
+
+        // Variáveis subtrações
         double resultadoSubtrai = mat.subrai(n1, n2);
+        double resultadoSubtrai2 = mat.subrai(n3, n4);
+
+        // Variáveis multiplica:
         double resultadoMultiplica = mat.multiplica(n1, n2);
+        double resultadoMultiplica2 = mat.multiplica(n3, n4);
+
+        // Variáveis divisões:
         double resultadoDivide = mat.divide(n1, n2);
+        double resultadoDivide2 = mat.divide(n3, n4);
 
         // Exibindo os resultados
         // Os resultados com duas casas decimais
+        // Somas:
         System.out.printf("Resultado da soma: %.2f\n", resultadoSoma);
+        System.out.printf("Resultado da soma 2: %.2f\n", resultadoSoma2);
+
+        // Subtrações:
         System.out.printf("Resultado da subtração: %.2f\n", resultadoSubtrai);
+        System.out.printf("Resultado da subtração 2: %.2f\n", resultadoSubtrai2);
+
+        // Multiplicações aqui:
         System.out.printf("Resultado da multiplicação: %.2f\n", resultadoMultiplica);
+        System.out.printf("Resultado da multiplicação 2: %.2f\n", resultadoMultiplica2);
+
+        // Divisões aqui:
         System.out.printf("Resultado da divisão: %.2f\n", resultadoDivide);
+        System.out.printf("Resultado da divisão 2: %.2f\n", resultadoDivide2);
 
         matematica m = new matematica();
         multiplicaCast mc = new multiplicaCast(m);
@@ -32,9 +58,9 @@ public class main {
         // Testando os métodos sobrecarregados com diferentes tipos de parâmetro;
 
         System.out.println(mc.multiplica(2, 3));  // int, int
-        System.out.println(mc.multiplica((int) 2.5f, (int) 3.2f));  // float, float
-        System.out.println(mc.multiplica((int) 2.5f, (int) 3.2));  // float, double
-        System.out.println(mc.multiplica((int) 2.5, (int) 3.2f));  // double, float
+        System.out.println(mc.multiplica(2.5f, 3.2f));  // float, float
+        System.out.println(mc.multiplica( 2.5f, 3.2));  // float, double
+        System.out.println(mc.multiplica( 2.5, 3.2f));  // double, float
         System.out.println(mc.multiplica(2L, 3));  // long, int
         System.out.println(mc.multiplica(2, 3L));  // int, long
         float f1 = Float.valueOf(2.5f);
--- 2Período/Algoritmos2/exercicios/Trabalhos/Final/Questões 1 - 2/classes/multiplicaCast.java
+++ 2Período/Algoritmos2/exercicios/Trabalhos/Final/Questões 1 - 2/classes/multiplicaCast.java
@@ -9,7 +9,7 @@ public class multiplicaCast {
     }
 
     public int multiplica(int n1, int n2) {
-        return (int) m.multiplica(n1, n2);
+        return (int) m.multiplica(n1, (double) n2);
     }
 
     public int multiplica(float n1, float n2) {

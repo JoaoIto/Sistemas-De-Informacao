2e8ede6e0b0c320aa5fc9428c316cd1768758919
João <joaovictorpfr@gmail.com>
2023-04-10 12:09:48 -0300

    Add statusAproved aluno and media, notas...

--- 2Período/Algoritmos2/exercicios/alg2-atv1/src/app/main.java
+++ 2Período/Algoritmos2/exercicios/alg2-atv1/src/app/main.java
@@ -20,15 +20,22 @@ public class main {
             System.out.println("Digite o nome do aluno:\n" +
                     "Digite o codigo de área do telefone \n " +
                     "Digite o número dele:\n "+
-                    "Digite o status dele: \n");
+                    "Digite o status dele: \n" +
+                    "Digite a nota 1 dele: \n " +
+                    "Digite a nota 2 dele: ");
 
             String nome = leitor.next();
             int codigoArea = leitor.nextInt();
             int numero = leitor.nextInt();
             String status = leitor.next();
+            double nota1 = leitor.nextDouble();
+            double nota2 = leitor.nextDouble();
 
             telefones[i] = new Telefone(codigoArea, numero);
-            alunos[i] = new Aluno(nome, telefones[i], status);
+            alunos[i] = new Aluno(nome, telefones[i], status, nota1, nota2);
+            alunos[i].setMedia(nota1, nota2);
+            double media = alunos[i].getMedia();
+            alunos[i].setAprovacao(media);
         }
 
         // Imprimindo
@@ -37,6 +44,9 @@ public class main {
             System.out.println("O telefone do aluno " + (i + 1) + " é: ");
             alunos[i].getTelefone();
             System.out.println("O status do aluno " + (i + 1) +" é: " + alunos[i].getStatus());
+            System.out.println("A média do aluno é: " + alunos[i].getMedia());
+            System.out.println("O aluno está" + alunos[i].getAprovacao());
+            System.out.println();
 }
     }
 }
--- 2Período/Algoritmos2/exercicios/alg2-atv1/src/classes/Aluno.java
+++ 2Período/Algoritmos2/exercicios/alg2-atv1/src/classes/Aluno.java
@@ -1,30 +1,68 @@
 package src.classes;
 
+import java.util.Scanner;
+import src.classes.Telefone;
+
 public class Aluno {
     private String nome;
-    //private String disciplina;
     private Telefone telefone;
-    //private double nota1;
-    //private double nota2;
-    //private double media;
+    private double nota1;
+    private double nota2;
+    private double media;
+    private String aprovacao;
     private String status;
 
     public Aluno() {
 
     }
 
-    public Aluno(String nome, Telefone telefone, String status) {
+    public Aluno(String nome, Telefone telefone, String status, double nota1, double nota2) {
         this.nome = nome;
-        //this.disciplina = disciplina;
         this.telefone = telefone;
-      //  this.nota1 = nota1;
-        //this.nota2 = nota2;
-        //this.media = (nota1 + nota2) / 2;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.media = (nota1 + nota2) / 2;
         this.status = status;
+    }
+
+    public String getAprovacao() {
+        return aprovacao;
+    }
+
+    public void setAprovacao(double media) {
+        if(this.media < 6){
+            this.aprovacao = "Reprovado";
+        }else{
+            this.aprovacao = "Aprovado";
+        }
     }
 
     public String getNome() {
         return nome;
+    }
+
+    public double getMedia() {
+        return ((getNota1() + getNota2()) / 2);
+    }
+
+    public void setMedia(double nota1, double nota2) {
+        this.media = (nota1 + nota2) / 2;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
     }
 
     public void setNome(String nome) {
@@ -39,9 +77,8 @@ public class Aluno {
         this.status = status;
     }
 
-    public void getTelefone() {
-        System.out.println(telefone.getCodigoArea());
-        System.out.println(telefone.getNumero());
+    public Telefone getTelefone() {
+        return telefone;
     }
 
     public void setTelefone(Telefone telefone) {

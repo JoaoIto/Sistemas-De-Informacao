5611b0889abe6262368abb7fcb824613784747e0
João <joaovictorpfr@gmail.com>
2023-04-10 14:50:57 -0300

    Add a base version

--- 2Período/Algoritmos2/exercicios/alg2-atv2/src/app/main.java
+++ 2Período/Algoritmos2/exercicios/alg2-atv2/src/app/main.java
@@ -1,6 +1,7 @@
 package src.app;
 
 import src.classes.Carro;
+import src.classes.Veiculo;
 
 public class main {
     public static void main(String[] args) {
@@ -8,13 +9,17 @@ public class main {
         System.out.println("Corrida de veículos!");
         System.out.println("Bem Vindo a corrida!");
 
-        Carro gol = new Carro("Gol 1.0", "Wolks", 2012, 200, 1200.0);
-        Carro unoDeEscadaBoTeto = new Carro("Fiat", "Uno 1.2", 2006, 250, 1600.0);
-        Carro ferrari = new Carro("Ferrari", "458 3.0 V8", 2016, 600, 900.0);
+        Veiculo veiculo1 = new Veiculo("Gol 1.0", "Wolks", 2012, 200, 1200.0);
+        Veiculo veiculo2 = new Veiculo("Fiat", "Uno 1.2", 2006, 250, 1600.0);
+        Veiculo veiculo3 = new Veiculo("Ferrari", "458 3.0 V8", 2016, 600, 900.0);
+
+        Carro gol = new Carro(veiculo1);
+        Carro unoEscada = new Carro(veiculo2);
+        Carro ferrari = new Carro(veiculo3);
 
         gol.imprimir();
         System.out.println("");
-        unoDeEscadaBoTeto.imprimir();
+        unoEscada.imprimir();
         System.out.println("");
         ferrari.imprimir();
         System.out.println("");
@@ -23,19 +28,19 @@ public class main {
             System.out.println("TACA-LE PAU NESSE CARINHO!");
             System.out.println(i);
             gol.acelerar();
-            unoDeEscadaBoTeto.acelerar();
+            unoEscada.acelerar();
             ferrari.acelerar();
             if(i == 6 || i == 11 || i == 15) {
                 System.out.println("VAI BATEEERRR");
                 gol.frear();
-                unoDeEscadaBoTeto.frear();
+                unoEscada.frear();
                 ferrari.frear();
             }
         }
 
         gol.imprimir();
         System.out.println("");
-        unoDeEscadaBoTeto.imprimir();
+        unoEscada.imprimir();
         System.out.println("");
         ferrari.imprimir();
         System.out.println("");
--- 2Período/Algoritmos2/exercicios/alg2-atv2/src/classes/Veiculos.java
+++
@@ -1,4 +0,0 @@
-package src.classes;
-
-public class Veiculos {
-}

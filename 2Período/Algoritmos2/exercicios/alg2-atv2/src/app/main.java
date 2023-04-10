08f7fd3a3b506246b9a27693bb7394d1e97070f2
João <joaovictorpfr@gmail.com>
2023-04-10 14:21:57 -0300

    main classes with 3 veicules

--- 2Período/Algoritmos2/exercicios/alg2-atv2/src/app/main.java
+++ 2Período/Algoritmos2/exercicios/alg2-atv2/src/app/main.java
@@ -7,5 +7,37 @@ public class main {
         System.out.println("Hello World!");
         System.out.println("Corrida de veículos!");
         System.out.println("Bem Vindo a corrida!");
+
+        Carro gol = new Carro("Gol 1.0", "Wolks", 2012, 200, 1200.0);
+        Carro unoDeEscadaBoTeto = new Carro("Fiat", "Uno 1.2", 2006, 250, 1600.0);
+        Carro ferrari = new Carro("Ferrari", "458 3.0 V8", 2016, 600, 900.0);
+
+        gol.imprimir();
+        System.out.println("");
+        unoDeEscadaBoTeto.imprimir();
+        System.out.println("");
+        ferrari.imprimir();
+        System.out.println("");
+
+        for(int i = 1; i < 20; i++) {
+            System.out.println("TACA-LE PAU NESSE CARINHO!");
+            System.out.println(i);
+            gol.acelerar();
+            unoDeEscadaBoTeto.acelerar();
+            ferrari.acelerar();
+            if(i == 6 || i == 11 || i == 15) {
+                System.out.println("VAI BATEEERRR");
+                gol.frear();
+                unoDeEscadaBoTeto.frear();
+                ferrari.frear();
+            }
+        }
+
+        gol.imprimir();
+        System.out.println("");
+        unoDeEscadaBoTeto.imprimir();
+        System.out.println("");
+        ferrari.imprimir();
+        System.out.println("");
     }
     }

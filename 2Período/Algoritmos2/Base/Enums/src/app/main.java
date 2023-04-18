c75362c3b99b2ceb95ff3bb5ddfc3ca76bae985f
JoaoIto <joaovictorpfr@gmail.com>
2023-04-18 09:06:38 -0300

    instance enums

--- 2Período/Algoritmos2/Base/Enums/src/app/main.java
+++ 2Período/Algoritmos2/Base/Enums/src/app/main.java
@@ -5,5 +5,9 @@ import src.enums.Colors;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Colors cor = Colors.AZUL;
+        System.out.println(cor);
+
     }
 }

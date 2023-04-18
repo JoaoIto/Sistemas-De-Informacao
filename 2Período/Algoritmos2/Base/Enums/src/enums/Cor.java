bfbc64490ffc08ce4c337ccc3590d44baf463b0b
JoaoIto <joaovictorpfr@gmail.com>
2023-04-18 09:40:03 -0300

    create a constructor enum cor

--- 2Período/Algoritmos2/Base/Enums/src/enums/Cor.java
+++ 2Período/Algoritmos2/Base/Enums/src/enums/Cor.java
@@ -1,9 +1,17 @@
 package src.enums;
 
 public enum Cor {
-    BRANCA,
-    PRETA,
-    AZUL,
-    AMARELA,
-    VERDE;
+    BRANCA(1, "branca"),
+    PRETA(2, "preta"),
+    AZUL(3, "azul"),
+    AMARELA(4,"amarela"),
+    VERDE(5, "verde");
+
+    private int id;
+    private String label;
+
+    private Cor(int id, String label){
+        this.id = id;
+        this.label = label;
+    }
 }

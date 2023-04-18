cfa33a4c9cd4e5d7f197ce6ab67f95bfaa1727d3
JoaoIto <joaovictorpfr@gmail.com>
2023-04-18 10:13:27 -0300

    add hexa to colors

--- 2Período/Algoritmos2/Base/Enums/src/enums/Cor.java
+++ 2Período/Algoritmos2/Base/Enums/src/enums/Cor.java
@@ -1,16 +1,17 @@
 package src.enums;
 
 public enum Cor {
-    BRANCA(1, "branca"),
-    PRETA(2, "preta"),
-    AZUL(3, "azul"),
-    AMARELA(4,"amarela"),
-    VERDE(5, "verde");
+    BRANCA(1, "branca", "#ffffff"),
+    PRETA(2, "preta", "#000000"),
+    AZUL(3, "azul", "#000fff"),
+    AMARELA(4,"amarela", "#FFFF00"),
+    VERDE(5, "verde", "#008000");
 
     private int id;
     private String label;
+    private String hexa;
 
-    private Cor(int id, String label){
+    private Cor(int id, String label, String hexa){
         this.id = id;
         this.label = label;
     }

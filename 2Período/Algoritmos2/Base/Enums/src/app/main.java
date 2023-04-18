b8815fa40050232432223d4c974f469caf5a48f8
JoaoIto <joaovictorpfr@gmail.com>
2023-04-18 10:09:17 -0300

    criando telefone com enum

--- 2Período/Algoritmos2/Base/Enums/src/app/main.java
+++ 2Período/Algoritmos2/Base/Enums/src/app/main.java
@@ -1,10 +1,15 @@
 package src.app;
 
+import src.classes.Telefone;
 import src.enums.Cor;
 
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Telefone iphone = new Telefone();
+        iphone.setModelo("Iphone12");
+        iphone.setColors(Cor.AZUL);
 
         for (Cor cor: Cor.values()) {
             System.out.println(cor.ordinal() + " - " + cor.name());

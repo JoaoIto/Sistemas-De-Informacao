6153f0d72bbeefecd60ce0adb5d601d5896f0a6f
JoaoIto <joaovictorpfr@gmail.com>
2023-05-02 08:42:00 -0300

    ArrayList

---
+++ 2Período/Algoritmos2/Base/Array/main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Array list: ");
+
+        Collection listCollection = new ArrayList<>();
+        List list = new ArrayList<>();
+    }
+}

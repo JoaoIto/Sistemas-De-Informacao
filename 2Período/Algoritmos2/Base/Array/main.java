07e33babf7b4788f5485f3be538ca0d30cece065
JoaoIto <joaovictorpfr@gmail.com>
2023-05-02 08:54:06 -0300

    readme

--- 2Período/Algoritmos2/Base/Array/main.java
+++ 2Período/Algoritmos2/Base/Array/main.java
@@ -6,8 +6,13 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Array list: ");
-
-        Collection listCollection = new ArrayList<>();
         List list = new ArrayList<>();
+
+        list.add(0, 1);
+        System.out.println(list);
+
+        list.set(0, 3);
+
+        System.out.println(list);
     }
 }

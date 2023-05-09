c3496fcfcd0e59cd3568960d0b2537dbdf00d55d
JoaoIto <joaovictorpfr@gmail.com>
2023-05-09 08:50:07 -0300

    class Pessoa

--- 2Período/Algoritmos2/Base/Collections/src/app/main.java
+++ 2Período/Algoritmos2/Base/Collections/src/app/main.java
@@ -1,5 +1,7 @@
 package src.app;
 
+import src.classes.Pessoa;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -8,12 +10,16 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+        Pessoa joao = new Pessoa("Joao", "24243278873", 17);
+        Pessoa mly = new Pessoa("Emily", "382394592", 18);
+        Pessoa janio = new Pessoa("Janio", "54935562935", 32);
+
         List<String> list = new ArrayList<>();
+        list.add("e");
         list.add("a");
+        list.add("d");
         list.add("b");
         list.add("c");
-        list.add("d");
-        list.add("e");
         list.add("f");
 
         for(String value: list){
---
+++ 2Período/Algoritmos2/Base/Collections/src/classes/Pessoa.java
@@ -0,0 +1,46 @@
+package src.classes;
+
+public class Pessoa {
+    private String name;
+    private String cpf;
+    private int age;
+
+    public Pessoa(String name, String cpf, int age) {
+        this.name = name;
+        this.cpf = cpf;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "name='" + name + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}

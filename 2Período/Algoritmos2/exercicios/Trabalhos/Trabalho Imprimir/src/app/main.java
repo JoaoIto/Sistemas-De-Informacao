5b1d0a20f6d2f42752c23ad000b837491d8e633d
João <joaovictorpfr@gmail.com>
2023-05-15 14:43:42 -0300

    ler e cadastrar e imprimir

--- 2Período/Algoritmos2/exercicios/Trabalhos/Trabalho Imprimir/src/app/main.java
+++ 2Período/Algoritmos2/exercicios/Trabalhos/Trabalho Imprimir/src/app/main.java
@@ -9,11 +9,51 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        Telefone telefone = new Telefone("55", "123456789");
-        Pessoa pessoa = new Pessoa("João", 25, telefone, Sexo.MASCULINO);
+        List<Pessoa> listaPessoas = new ArrayList<>();
 
-        telefone.imprimir();
-        pessoa.imprimir();
+        for (int i = 0; i < 2; i++) {
+            Pessoa pessoa = lerDados();
+            listaPessoas.add(pessoa);
+        }
+
+        imprimirLista(listaPessoas);
+    }
+
+    public static Pessoa lerDados() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o nome da pessoa:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite a idade da pessoa:");
+        int idade = scanner.nextInt();
+
+        scanner.nextLine(); // Consumir a quebra de linha após a leitura do int
+
+        System.out.println("Digite o código de área do telefone:");
+        String codigoArea = scanner.nextLine();
+
+        System.out.println("Digite o número do telefone:");
+        String numero = scanner.nextLine();
+
+        System.out.println("Digite o sexo da pessoa (MASCULINO ou FEMININO):");
+        String sexoStr = scanner.nextLine();
+
+        Sexo sexo = Sexo.valueOf(sexoStr.toUpperCase());
+
+        Telefone telefone = new Telefone(codigoArea, numero);
+
+        return new Pessoa(nome, idade, telefone, sexo);
+    }
+
+    public static void imprimirLista(List<Pessoa> lista) {
+        for (Pessoa pessoa : lista) {
+            System.out.println("Nome: " + pessoa.getNome());
+            System.out.println("Idade: " + pessoa.getIdade());
+            System.out.println("Telefone: +" + pessoa.getTelefone().getCodigoArea() + " " + pessoa.getTelefone().getNumero());
+            System.out.println("Sexo: " + pessoa.getSexo());
+            System.out.println("----------------------------------");
+        }
     }
 }
 

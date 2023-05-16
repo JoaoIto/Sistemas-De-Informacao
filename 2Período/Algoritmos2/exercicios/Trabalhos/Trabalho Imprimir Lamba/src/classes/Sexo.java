f1ae5d78843c47a3b6f0d312b1e2bd44332a39fb
João <joaovictorpfr@gmail.com>
2023-05-16 08:36:59 -0300

    add valueOf

--- 2Período/Algoritmos2/exercicios/Trabalhos/Trabalho Imprimir Lamba/src/classes/Sexo.java
+++ 2Período/Algoritmos2/exercicios/Trabalhos/Trabalho Imprimir Lamba/src/classes/Sexo.java
@@ -4,19 +4,28 @@ public enum Sexo {
     MASCULINO(0,"Masculino"),
     FEMININO(1, "Femino");
 
-    private final int valor;
+    private final Integer valor;
     private final String label;
 
-    Sexo(int valor, String label) {
-        this.valor = valor;
+    Sexo(int id, String label) {
+        this.valor = id;
         this.label = label;
     }
 
-    public int getValor() {
+    public Integer getId() {
         return valor;
     }
 
     public String getLabel() {
         return label;
+    }
+
+    public static Sexo valueOf(Integer id){
+        if(id != null)
+        for (Sexo sexo : Sexo.values()) {
+            if(sexo.getId().equals(id))
+                return sexo;
+        }
+        return null;
     }
 }

8e54038812e190c02970bca74fd998fe1bbd9590
João <joaovictorpfr@gmail.com>
2023-05-23 15:45:16 -0300

    add c++ code base

--- 2Período/Arquitetura de Comp/Projeto Arduíno/arduino.ino
+++ 2Período/Arquitetura de Comp/Projeto Arduíno/arduino.ino
@@ -1,96 +1,65 @@
-#include <Keypad.h>
+#include <Keypad.h> // BIBLIOTECA PARA O FUNCIONAMENTO DO TECLADO DE MEMBRANA
+#include <Servo.h>  // BIBLIOTECA PARA O FUNCIONAMENTO DO SERVO
 
-// Criando a senha do terminal:
-char password[] = "dc0#*";
-
-// Definindo número de linhas e colunas do teclado:
-const byte ROWS = 4;
-const byte COLS = 4;
-
-// Definindo caracteres do teclado externo:
-char keys[ROWS][COLS] = {
+Servo servo_Motor;        // OBJETO DO TIPO SERVO
+char *password = "123";   // SENHA CORRETA PARA DESTRANCAR A FECHADURA
+int position = 0;         // VARIÁVEL PARA LEITURA DE POSIÇÃO DA TECLA PRESSIONADA
+const byte ROWS = 4;      // NUMERO DE LINHAS DO TECLADO
+const byte COLS = 4;      // NUMERO DE COLUNAS DO TECLADO
+char keys[ROWS][COLS] = { // DECLARAÇÃO DOS NUMEROS, LETRAS E CARACTERES DO TECLADO
     {'1', '2', '3', 'A'},
     {'4', '5', '6', 'B'},
     {'7', '8', '9', 'C'},
     {'*', '0', '#', 'D'}};
 
-// Definindo pinos de conexão das linhas e colunas no Arduino:
-byte rowPins[ROWS] = {11, 10, 9, 8};
-byte colPins[COLS] = {7, 6, 5, 4};
+byte rowPins[ROWS] = {9, 8, 7, 6}; // PINOS DE CONEXAO DAS LINHAS DO TECLADO
+byte colPins[COLS] = {5, 4, 3, 2}; // PINOS DE CONEXAO DAS COLUNAS DO TECLADO
 
-// Definindo pinos dos componentes:
-int ledVermelho = 12;
-int ledVerde = 13;
-int releFechadura = 3;
-int buzzer = 2;
-
-// Definindo tempos de espera:
-int tempoBuzz = 50;
-int tempoAberto = 2000;
-
-// Criando objeto para leitura das teclas:
+// AS VARIAVEIS rowPins E colPins RECEBERÃO O VALOR DE LEITURA DOS PINOS DAS LINHAS E COLUNAS RESPECTIVAMENTE
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-// Função para trancar ou destrancar a porta:
-void estadoPorta(bool trancada)
-{
-    if (trancada)
-    {
-        digitalWrite(ledVermelho, HIGH);
-        digitalWrite(ledVerde, LOW);
-        digitalWrite(releFechadura, HIGH);
-    }
-    else
-    {
-        digitalWrite(ledVermelho, LOW);
-        digitalWrite(ledVerde, HIGH);
-        digitalWrite(releFechadura, LOW);
-        delay(tempoAberto);
-        digitalWrite(releFechadura, HIGH);
-    }
-}
+int ledVermelho = 12; // PINO EM QUE ESTÁ CONECTADO O LED VERMELHO
+int ledVerde = 11;    // PINO EM QUE ESTÁ CONECTADO O LED VERDE
 
 void setup()
 {
-    pinMode(LED_BUILTIN, OUTPUT);
-    estadoPorta(true); // Estado inicial da porta, (trancada);
+    pinMode(ledVermelho, OUTPUT); // DECLARA O PINO COMO SAÍDA
+    pinMode(ledVerde, OUTPUT);    // DECLARA O PINO COMO SAÍDA
 
-    // Estados iniciais para pinos dos componentes:
-    pinMode(ledVermelho, OUTPUT);
-    pinMode(ledVerde, OUTPUT);
-    pinMode(releFechadura, OUTPUT);
-    pinMode(buzzer, OUTPUT);
+    servo_Motor.attach(13); // PINO DE CONTROLE DO SERVO MOTOR
+    setLocked(true);        // ESTADO INICIAL DA FECHADURA (TRANCADA)
 }
 
 void loop()
 {
-    // Estado base para o arduíno estar funcionando caso esteja "rodando"
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
-
-    char key = keypad.getKey(); // Lendo a tecla pressionada
-
-    if (key != NO_KEY)
-    { // Se uma tecla foi pressionada
-        digitalWrite(buzzer, HIGH);
-        delay(tempoBuzz);
-        digitalWrite(buzzer, LOW); // Acionando o buzzer para indicar que a tecla foi pressionada
-
-        // Verificando se a tecla pressionada faz parte da senha:
-        if (key == password[position])
-        {
-            position++;
-            if (position == sizeof(password) - 1)
-            {                       // Se a senha foi digitada corretamente
-                estadoPorta(false); // Destrancando a porta
-                position = 0;
-            }
-        }
-        else
-        {
-            position = 0; // Reiniciando a posição da senha
-        }
+    char key = keypad.getKey(); // LEITURA DAS TECLAS PRESSIONADAS
+    if (key == '*' || key == '#')
+    {                    // SE A TECLA PRESSIONADA POR IGUAL A CARACTERE "*" OU "#", FAZ
+        position = 0;    // POSIÇÃO DE LEITURA DA TECLA PRESSIONADA INICIA EM 0
+        setLocked(true); // FECHADURA TRANCADA
+    }
+    if (key == password[position])
+    {               // SE A TECLA PRESSIONADA CORRESPONDER A SEQUÊNCIA DA SENHA, FAZ
+        position++; // PULA PARA A PRÓXIMA POSIÇÃO
+    }
+    if (position == 3)
+    {                     // QUANTIDADE DE DIGITOS QUE A SENHA POSSUI
+        setLocked(false); // FECHADURA DESTRANCADA
+    }
+    delay(100); // INTERVALO DE 100 MILISSEGUNDOS
+}
+void setLocked(int locked)
+{ // TRATANDO O ESTADO DA FECHADURA
+    if (locked)
+    {                                    // SE FECHADURA TRANCADA, FAZ
+        digitalWrite(ledVermelho, HIGH); // LED VERMELHO ACENDE
+        digitalWrite(ledVerde, LOW);     // LED VERDE APAGA
+        servo_Motor.write(0);            // POSIÇÃO DO SERVO FICA EM 0º (FECHADURA TRANCADA)
+    }
+    else
+    {                                   // SENÃO, FAZ
+        digitalWrite(ledVerde, HIGH);   // LED VERDE ACENDE
+        digitalWrite(ledVermelho, LOW); // LED VERMELHO APAGA
+        servo_Motor.write(82);          // SERVO GIRA A 82º (FECHADURA DESTRANCADA)
     }
 }

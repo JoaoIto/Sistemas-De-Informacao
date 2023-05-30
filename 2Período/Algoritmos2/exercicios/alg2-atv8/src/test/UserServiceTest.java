1ac7245347a8dead9c83d628144808a5f6e41fb6
JoaoIto <joaovictorpfr@gmail.com>
2023-05-30 09:44:56 -0300

    add base tests information

--- 2Período/Algoritmos2/exercicios/alg2-atv8/src/test/UserServiceTest.java
+++ 2Período/Algoritmos2/exercicios/alg2-atv8/src/test/UserServiceTest.java
@@ -7,9 +7,19 @@ import src.classes.UserService;
 import static org.junit.jupiter.api.Assertions.*;
 public class UserServiceTest {
     @Test
-    void test(){
+    void testuser(){
         UserService service = new UserService();
         User user = service.login("joao@email.com", "2308");
+        assertNotNull(user);
+    }
+
+    @Test
+    void verifyInformation(){
+        UserService service = new UserService();
+        User user = service.login("joao@email.com", "2308");
+
+        user.getNome().equals("Joao");
+
         assertNotNull(user);
     }
 }

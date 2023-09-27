b3ee0af74b7610a0b978bf5b840460fc3dfbd0f4
JoaoIto <joaovictorpfr@gmail.com>
2023-09-27 05:18:16 -0300

    delete quarkus code demo

--- 3Período/Topicos em Programacao/Quarkkus/demo1/src/test/java/com/topicos/ExampleResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.topicos;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class ExampleResourceIT extends ExampleResourceTest {
-    // Execute the same tests but in packaged mode.
-}
